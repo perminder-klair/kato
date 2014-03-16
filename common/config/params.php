4156c3f64becc5c8bcf3f4174c7ff9be292404fe
parminder <perminder.klair@gmail.com>
2014-03-16 17:27:04 +0000

    Added max upload size param

--- common/config/params.php
+++ common/config/params.php
@@ -4,5 +4,6 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    'maxUploadSize' => 2097152,
     'uploadPath' => dirname(dirname(__DIR__)) . '/frontend/web/files/',
 ];
