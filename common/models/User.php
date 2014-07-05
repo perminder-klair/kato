a1fcdeef0b143e44ae7440e5e478267e78d173c6
Parminder Klair <perminder.klair@gmail.com>
2014-07-05 14:18:50 +0100

    Update User.php
    fixed after parent error

--- common/models/User.php
+++ common/models/User.php
@@ -201,9 +201,9 @@ class User extends ActiveRecord implements IdentityInterface
     /**
      * This method is called at the end of inserting or updating a record.
      */
-    public function afterSave($insert = true)
+    public function afterSave($insert = true, $changedAttributes = [])
     {
-        if (parent::afterSave($insert, [])) {
+        if (parent::afterSave($insert, $changedAttributes)) {
             $this->updateRole($this->role);
             $this->save();
         }
