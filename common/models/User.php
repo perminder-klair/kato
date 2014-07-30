1657e449f12ab7f3348800b97d53a1ff1f88af86
Parminder Klair <perminder.klair@gmail.com>
2014-07-30 16:37:22 +0100

    Update User.php

--- common/models/User.php
+++ common/models/User.php
@@ -274,7 +274,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public function generatePasswordResetToken()
     {
-        $this->password_reset_token = Security::generateRandomKey() . '_' . time();
+        $this->password_reset_token = Yii::$app->security->generateRandomString() . '_' . time();
     }
 
     /**
