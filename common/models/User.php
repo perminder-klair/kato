fa2fbfefcd29cd0a4cdff89a1c75daff57803d67
Parminder Klair <perminder.klair@gmail.com>
2014-07-30 16:36:44 +0100

    Update User.php

--- common/models/User.php
+++ common/models/User.php
@@ -266,7 +266,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public function generateAuthKey()
     {
-        $this->auth_key = Security::generateRandomKey();
+        $this->auth_key = Yii::$app->security->generateRandomString();
     }
 
     /**
