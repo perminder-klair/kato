94f50a690e8c5126ba8d15e14e0839c2b0a03fa9
Parminder Klair <perminder.klair@gmail.com>
2014-07-31 15:47:26 +0100

    Update main.php

--- backend/config/main.php
+++ backend/config/main.php
@@ -14,10 +14,11 @@ return [
 	'components' => [
         'user' => [
             'identityClass' => 'common\models\User',
+            'loginUrl' => ['/admin/login'],
         ],
-		'errorHandler' => [
-			'errorAction' => 'site/error',
-		],
+	'errorHandler' => [
+		'errorAction' => 'site/error',
+	],
         'urlManager' => [
             'class' => 'kato\components\UrlManager',
             'adminUrl' => 'admin',
