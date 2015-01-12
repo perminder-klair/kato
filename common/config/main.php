a8a26efa3ef86b43b4837a6a63840fa8a721c04c
Parminder Klair <perminder.klair@gmail.com>
2015-01-12 14:33:36 +0000

    Update main.php
    load tags module from kato core

--- common/config/main.php
+++ common/config/main.php
@@ -21,6 +21,9 @@ return [
         'media' => [
             'class' => 'kato\modules\media\Media',
         ],
+        'tag' => [
+            'class' => 'kato\modules\Tag',
+        ],
     ],
     'components' => [
         'assetManager' => [
